3a8557e77caf434565d4e9ef5a5c5f70e4befa4d
KaiPonel <82947121+KaiPonel@users.noreply.github.com>
2022-08-15 16:21:52 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'UMD-Driverless Simulator'
+copyright = '2022, UMD-Racing'
+author = 'UMD-Racing'
 
 release = '0.1'
 version = '0.1.0'
